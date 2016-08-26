30147d025e671081fc2bdb38f4a8862a12a47cac
yongpansong <yongpansong@gmail.com>
2016-08-26 09:42:41 +0800

    print file's name and line's number

--- DyVoiceSDK/jni/utils/dyutils.h
+++ DyVoiceSDK/jni/utils/dyutils.h
@@ -12,11 +12,25 @@
 
 #define TAG "DYVOICE"
 
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__)
-#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__)
+#define LOGD(format, args...) do { \
+  __android_log_print(ANDROID_LOG_DEBUG, TAG, "[D][%.20s:%d]:"format"\n", __FILE__+strlen(__FILE__)-20, __LINE__, ##args); \
+} while (0)
+
+#define LOGI(format, args...) do { \
+  __android_log_print(ANDROID_LOG_INFO, TAG, "[I][%.20s:%d]:"format"\n", __FILE__+strlen(__FILE__)-20, __LINE__, ##args); \
+} while (0)
+
+#define LOGW(format, args...) do { \
+  __android_log_print(ANDROID_LOG_WARNNING, TAG, "[W][%.20s:%d]:"format"\n", __FILE__+strlen(__FILE__)-20, __LINE__, ##args); \
+} while (0)
+
+#define LOGE(format, args...) do { \
+  __android_log_print(ANDROID_LOG_ERROR, TAG, "[E][%.20s:%d]:"format"\n", __FILE__+strlen(__FILE__)-20, __LINE__, ##args); \
+} while (0)
+
+#define LOGF(format, args...) do { \
+  __android_log_print(ANDROID_LOG_FATAL, TAG, "[F][%.20s:%d]:"format"\n", __FILE__+strlen(__FILE__)-20, __LINE__, ##args); \
+} while (0)
 
 unsigned long getTimestamp();
 
